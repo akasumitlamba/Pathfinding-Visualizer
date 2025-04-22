d556880885d734fa59d3fd63f35d9c1466aa6f04
sumit kumar <85924848+akasumitlamba@users.noreply.github.com>
2025-04-22 15:06:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ app.use("/public", express.static(__dirname + "/public"))
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 })
-
 app.listen(1337, () => {
   console.log("The server is up and running!");
 });
